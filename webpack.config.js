a1e6b97d33eec63b94a973518b77296809932fa6
gurandalemonjava <guralemo@gmail.com>
2023-07-31 16:42:37 +0400

    initial change

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,67 @@
 const path = require("path");
-module.exports = {
-  mode: "development",
-  entry: path.resolve(__dirname, "./src/email-validator.js"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
+const CopyPlugin = require("copy-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    mode: isProduction ? "production" : "development",
+    entry: {
+      main: "./src/main.js",
+      style: "./styles/style.css",
+    },
+    output: {
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
           },
         },
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|jpg|gif)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+    devServer: {
+      static: path.join(__dirname, "dist"),
+    },
+    plugins: [
+      new CopyPlugin({
+        patterns: [
+          { from: "src", to: "" }, // Copy src contents to dist
+          { from: "styles/style.css", to: "styles" }, // Copy style.css to dist/styles
+          { from: "assets/images", to: "assets/images" }, // Copy logo images to dist/assets/images
+        ],
+      }),
     ],
-  },
+    optimization: {
+      minimizer: isProduction
+        ? [
+            new TerserPlugin({
+              terserOptions: {
+                format: {
+                  comments: false,
+                },
+              },
+              extractComments: false,
+            }),
+          ]
+        : [],
+    },
+  };
 };
