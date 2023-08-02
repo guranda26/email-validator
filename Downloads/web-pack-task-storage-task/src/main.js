9853869299b171dd4d82bc35e6312035f61e1b37
gurandalemonjava <guralemo@gmail.com>
2023-08-02 12:00:39 +0400

    .

---
+++ Downloads/web-pack-task-storage-task/src/main.js
@@ -0,0 +1,85 @@
+import isValid from "./email-validator";
+import "../styles/style.css";
+
+class Section {
+  constructor(title, buttonText) {
+    this.title = title;
+    this.buttonText = buttonText;
+    this.sectionElement = this.joinProgramSection();
+  }
+
+  joinProgramSection() {
+    const joinProgram = document.createElement("section");
+    joinProgram.className = "app-section app-section--join-our-program";
+    const title = document.createElement("h2");
+    title.className = "app-title";
+    title.textContent = `${this.title}`;
+    const subtitle = document.createElement("h3");
+    subtitle.className = "app-subtitle";
+    subtitle.textContent =
+      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+    const form = document.createElement("form");
+    form.id = "subscribeForm";
+    const inputEmail = document.createElement("input");
+
+    inputEmail.type = "email";
+    inputEmail.className = "input-email";
+    inputEmail.placeholder = "email";
+    inputEmail.id = "email";
+    inputEmail.name = "email";
+
+    const submitButton = document.createElement("button");
+    submitButton.type = "submit";
+    submitButton.className =
+      "app-section__button subscribe-button app-section__button--read-more";
+    submitButton.textContent = this.buttonText;
+
+    form.appendChild(inputEmail);
+    form.appendChild(submitButton);
+    joinProgram.appendChild(title);
+    joinProgram.appendChild(subtitle);
+    joinProgram.appendChild(form);
+
+    const appContainer = document.getElementById("app-container");
+    appContainer.insertBefore(
+      joinProgram,
+      appContainer.querySelector("footer")
+    );
+
+    submitButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      const email = inputEmail.value;
+      const isEmailValid = isValid(email);
+
+      if (isEmailValid) {
+        if (submitButton.textContent === "Subscribe") {
+          // Subscribe functionality
+          localStorage.setItem("subscriptionEmail", email);
+          submitButton.textContent = "Unsubscribe";
+        } else {
+          // Unsubscribe functionality
+          localStorage.removeItem("subscriptionEmail");
+          inputEmail.value = "";
+          submitButton.textContent = "Subscribe";
+        }
+      } else {
+        alert("Invalid email address. Please try again");
+      }
+    });
+
+    const savedEmail = localStorage.getItem("subscriptionEmail");
+    if (savedEmail) {
+      inputEmail.value = savedEmail;
+      submitButton.textContent = "Unsubscribe";
+    }
+
+    return joinProgram;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const sectionCreator = new Section("Join Our Program", "Subscribe");
+  const appContainer = document.getElementById("app-container");
+  appContainer.insertBefore(joinProgram, appContainer.querySelector("footer"));
+});
---
+++ Downloads/web-pack-task-storage-task/webpack.config.js
@@ -0,0 +1,80 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    mode: isProduction ? "production" : "development",
+    entry: {
+      main: "./src/main.js",
+      // style: "./styles/style.css",
+    },
+    output: {
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+      rules: [
+        {
+          enforce: "pre",
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          loader: "eslint-loader",
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|jpg|gif)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+    devServer: {
+      static: path.join(__dirname, "dist"),
+    },
+    plugins: [
+      new CopyPlugin({
+        patterns: [
+          { from: "src", to: "" }, // Copy src contents to dist
+          { from: "styles/style.css", to: "styles" }, // Copy style.css to dist/styles
+          { from: "assets/images", to: "assets/images" }, // Copy logo images to dist/assets/images
+        ],
+      }),
+
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        inject: "body",
+      }),
+      // ...
+    ],
+    optimization: {
+      minimizer: isProduction
+        ? [
+            new TerserPlugin({
+              terserOptions: {
+                format: {
+                  comments: false,
+                },
+              },
+              extractComments: false,
+            }),
+          ]
+        : [],
+    },
+  };
+};
